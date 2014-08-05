1da1c4ab97a28a40b9ebb37305a1515816c6f443
Daniel Gonzalez <dyg2104@gmail.com>
2014-08-05 17:41:47 -0400

    Wrote methods to route an http request in the correct direction. Began writing a method to run those routes.

--- skeleton/lib/phase6/router.rb
+++ skeleton/lib/phase6/router.rb
@@ -3,10 +3,21 @@ module Phase6
     attr_reader :pattern, :http_method, :controller_class, :action_name
 
     def initialize(pattern, http_method, controller_class, action_name)
+      @pattern = pattern
+      @http_method = http_method
+      @controller_class = controller_class
+      @action_name = action_name
     end
 
     # checks if pattern matches path and method matches request method
     def matches?(req)
+      if !self.pattern.match(req.path).nil?
+        if self.http_method == req.request_method.downcase.to_sym
+          return true
+        end
+      end
+
+      return false
     end
 
     # use pattern to pull out route params (save for later?)
@@ -19,10 +30,12 @@ module Phase6
     attr_reader :routes
 
     def initialize
+      @routes = []
     end
 
     # simply adds a new route to the list of routes
     def add_route(pattern, method, controller_class, action_name)
+      @routes << Route.new(pattern, method, controller_class, action_name)
     end
 
     # evaluate the proc in the context of the instance
@@ -33,10 +46,17 @@ module Phase6
     # make each of these methods that
     # when called add route
     [:get, :post, :put, :delete].each do |http_method|
+      define_method(http_method) do |pattern, controller_class, action_name|
+        add_route(pattern, http_method, controller_class, action_name)
+      end
     end
 
     # should return the route that matches this request
     def match(req)
+      routes.each do |route|
+        return route if route.matches?(req)
+      end
+      return nil
     end
 
     # either throw 404 or call run on a matched route
